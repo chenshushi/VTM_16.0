d81029af2917a3f7197928d3389d3ff4a7037196
vip-1 <chenshushi98@163.com>
2023-05-07 12:10:45 +0800

    [Env] Adjust the .gitignore and run.sh

--- runVTM/VTM.sh
+++ runVTM/VTM.sh
@@ -29,59 +29,91 @@
 #*** PARAMETER *****************************************************************
 # directory or file name
 CSTR_CDC="VTM"
-CSTR_DIR_SRC="/mnt/e/DOWNLOAD/SEQUENCE/HM_YUV100to131/SEQUENCE/HM/bitDepth_8"    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
+CSTR_DIR_SRC="/media/vip/F69022E79022AE55/chenss/SEQUENCE_VTM/VVC"    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
 CSTR_DIR_DST="dump"                                       #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
 CSTR_LOG_RUN="$CSTR_DIR_DST/runs.log"                     #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
 CSTR_LOG_JOB="$CSTR_DIR_DST/jobs.log"                     #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
 CSTR_LOG_RLT_PSNR="$CSTR_DIR_DST/result.log"              #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
+CSTR_LOG_RLT_time="$CSTR_DIR_DST/result_time.log"         #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
+CSTR_LOG_RLT_ENCT="$CSTR_DIR_DST/encTime.log"             #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
 CSTR_LOG_RLT_BDRT="$CSTR_DIR_DST/bdRate.log"              #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
 
 # sequence
 #   name               frame fps width height depth
 LIST_SEQ_AVAILABLE=(
-    "BasketballPass"   501   50  416   240    8
-    "BQSquare"         601   60  416   240    8
-    "BlowingBubbles"   501   50  416   240    8
-    "RaceHorses"       300   30  416   240    8
-    "BasketballDrill"  501   50  832   480    8
-    "BQMall"           601   60  832   480    8
-    "PartyScene"       501   50  832   480    8
-    "RaceHorsesC"      300   30  832   480    8
-    "FourPeople"       600   60  1280  720    8
-    "Johnny"           600   60  1280  720    8
-    "KristenAndSara"   600   60  1280  720    8
-    "Kimono"           240   24  1920  1080   8
-    "ParkScene"        240   24  1920  1080   8
-    "Cactus"           500   50  1920  1080   8
-    "BasketballDrive"  501   50  1920  1080   8
-    "BQTerrace"        601   60  1920  1080   8
-    "Traffic"          150   30  2560  1600   8
-    "PeopleOnStreet"   150   30  2560  1600   8
+    # A1
+    "FoodMarket4"           300     60      3840    2160    10
+    "Tango2"                294     60      3840    2160    10
+    "Campfire"              300     30      3840    2160    10
+    # A2
+    "ParkRunning3"          300     50      3840    2160    10
+    "CatRobot"              300     60      3840    2160    10
+    "DaylightRoad2"         300     60      3840    2160    10
+    # B
+    "MarketPlace"           600     60      1920    1080    10
+    "BasketballDrive"       500     50      1920    1080    8
+    "BQTerrace"             600     60      1920    1080    8
+    "Cactus"                500     50      1920    1080    8
+    "RitualDance"           600     60      1920    1080    10
+    # C
+    "BasketballDrill"       500     50      832     480     8
+    "BQMall"                600     60      832     480     8
+    "PartyScene"            500     50      832     480     8
+    "RaceHorsesC"           300     30      832     480     8
+    # D
+    "BasketballPass"        500     50      416     240     8
+    "BlowingBubbles"        500     50      416     240     8
+    "BQSquare"              600     60      416     240     8
+    "RaceHorses"            300     30      416     240     8
+    # E
+    "FourPeople"            600     60      1280    720     8
+    "Johnny"                600     60      1280    720     8
+    "KristenAndSara"        600     60      1280    720     8
+    # screen content
+    "ArenaOfValor"          600     60      1920    1080    8
+    "SlideEditing"          300     30      1280    720     8
+    "SlideShow"             500     20      1280    720     8
+    "BasketballDrillText"   500     50      832     480     8
 )
 LIST_SEQ=(
-    "BasketballPass"   8     60  416   240    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "BQSquare"         8     60  416   240    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "BlowingBubbles"   8     60  416   240    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "RaceHorses"       8     60  416   240    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "BasketballDrill"  8     60  832   480    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "BQMall"           8     60  832   480    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "PartyScene"       8     60  832   480    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "RaceHorsesC"      8     60  832   480    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "FourPeople"       8     60  1280  720    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "Johnny"           8     60  1280  720    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "KristenAndSara"   8     60  1280  720    8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "Kimono"           8     60  1920  1080   8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "ParkScene"        8     60  1920  1080   8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "Cactus"           8     60  1920  1080   8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "BasketballDrive"  8     60  1920  1080   8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "BQTerrace"        8     60  1920  1080   8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "Traffic"          8     60  2560  1600   8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-    "PeopleOnStreet"   8     60  2560  1600   8    #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
+    # # A1
+    # "FoodMarket4"           100     60      3840    2160    10
+    # "Tango2"                100     60      3840    2160    10
+    # "Campfire"              100     30      3840    2160    10
+    # # A2
+    # "ParkRunning3"          100     50      3840    2160    10
+    # "CatRobot"              100     60      3840    2160    10
+    # "DaylightRoad2"         100     60      3840    2160    10
+    # # B
+    # "MarketPlace"           100     60      1920    1080    10
+    # "BasketballDrive"       100     50      1920    1080    8
+    # "BQTerrace"             100     60      1920    1080    8
+    # "Cactus"                100     50      1920    1080    8
+    # "RitualDance"           100     60      1920    1080    10
+    # # # C
+    # "BasketballDrill"       100     50      832     480     8
+    # "BQMall"                100     60      832     480     8
+    # "PartyScene"            100     50      832     480     8
+    # "RaceHorsesC"           100     30      832     480     8
+    # # # D
+    "BasketballPass"        100     50      416     240     8
+    # "BlowingBubbles"        100     50      416     240     8
+    # "BQSquare"              100     60      416     240     8
+    # "RaceHorses"            100     30      416     240     8
+    # # E
+    # "FourPeople"            100     60      1280    720     8
+    # "Johnny"                100     60      1280    720     8
+    # "KristenAndSara"        100     60      1280    720     8
+    # # screen content
+    # "ArenaOfValor"          100     60      1920    1080    8
+    # "SlideEditing"          100     30      1280    720     8
+    # "SlideShow"             100     20      1280    720     8
+    # "BasketballDrillText"   100     50      832     480     8
 )
 
 # encoder
 LIST_DAT_Q_P=($(seq 22 5 37))                      #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
-DATA_PRD_INTRA=10000                               #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
+DATA_PRD_INTRA=-1                                  #REVERT_THIS_ITEM_BEFORE_YOU_COMMIT
 
 
 #*** MAIN BODY *****************************************************************
@@ -90,12 +122,13 @@ DATA_PRD_INTRA=10000                               #REVERT_THIS_ITEM_BEFORE_YOU_
 mkdir -p $CSTR_DIR_DST
 rm -rf $CSTR_DIR_DST/*
 rm -rf $CSTR_LOG_RLT_PSNR
+rm -rf $CSTR_LOG_RLT_time
+printf "%-15s %-13s\n" "encTime" "test_Sequence" >> $CSTR_LOG_RLT_time
 printf "%-51s %-51s %-51s %s\n" "average" "I frame" "P frame" "B frame" >> $CSTR_LOG_RLT_PSNR
 printf "%-13s \t%-7s \t%-7s \t%-7s \t"    "bitrate(kb/s)" "psnr(Y)"    "psnr(U)"    "psnr(V)"    >> $CSTR_LOG_RLT_PSNR
 printf "%-13s \t%-7s \t%-7s \t%-7s \t"    "bitrate(kb/s)" "psnr(Y)"    "psnr(U)"    "psnr(V)"    >> $CSTR_LOG_RLT_PSNR
 printf "%-13s \t%-7s \t%-7s \t%-7s \t"    "bitrate(kb/s)" "psnr(Y)"    "psnr(U)"    "psnr(V)"    >> $CSTR_LOG_RLT_PSNR
-printf "%-13s \t%-7s \t%-7s \t%-7s\n"     "bitrate(kb/s)" "psnr(Y)"    "psnr(U)"    "psnr(V)"    >> $CSTR_LOG_RLT_PSNR
-
+printf "%-13s \t%-7s \t%-7s \t%-7s\n"    "bitrate(kb/s)" "psnr(Y)"    "psnr(U)"    "psnr(V)"    >> $CSTR_LOG_RLT_PSNR
 # note down the current time
 timeBgnAll=$(date +%s)
 
@@ -115,7 +148,8 @@ do
     DATA_PXL_WD=${LIST_SEQ[$cntSeq]}; cntSeq=$((cntSeq + 1))
 
     # derive parameter
-    CSTR_SRC=$CSTR_DIR_SRC/$CSTR_SEQ/$CSTR_SEQ
+    CSTR_SRC=$CSTR_DIR_SRC/$CSTR_SEQ/
+    CSTR_SRC_YUV=$(ls $CSTR_DIR_SRC/$CSTR_SEQ)
 
     # log
     echo ""
@@ -147,7 +181,7 @@ do
                                                                       \
             -c                  cfg/encoder_lowdelay_P_vtm.cfg        \
                                                                       \
-                    --InputFile=${CSTR_SRC}.yuv                       \
+                    --InputFile=${CSTR_SRC}/${CSTR_SRC_YUV}           \
             --FramesToBeEncoded=${NUMB_FRA}                           \
                   --SourceWidth=${SIZE_FRA_X}                         \
                  --SourceHeight=${SIZE_FRA_Y}                         \
@@ -155,7 +189,7 @@ do
                 --InputBitDepth=${DATA_PXL_WD}                        \
                     --FrameRate=${DATA_FPS}                           \
                            --QP=${DATA_Q_P}                           \
-                  --IntraPeriod=$((DATA_PRD_INTRA + 1))               \
+                  --IntraPeriod=${DATA_PRD_INTRA}               \
                 --BitstreamFile=${CSTR_DIR_DST_FUL}${CSTR_CDC}.bin    \
                                                                       \
         >& ${CSTR_DIR_DST_FUL}${CSTR_CDC}.log &
@@ -200,8 +234,14 @@ do
         # update info (psnr)
         ./script/getInfo.py ${CSTR_DIR_DST_FUL}${CSTR_CDC}.log >> $CSTR_LOG_RLT_PSNR
         echo "$CSTR_DIR_SUB" >> $CSTR_LOG_RLT_PSNR
+        # process time
+        chmod +x ./script/getTime.py && ./script/getTime.py ${CSTR_DIR_DST_FUL}${CSTR_CDC}.log >> $CSTR_LOG_RLT_time
+        echo "$CSTR_DIR_SUB" >> $CSTR_LOG_RLT_time
     done
 
     # update bd rate
-    ./script/getBdRate.py script/anchor.log $CSTR_LOG_RLT_PSNR > $CSTR_LOG_RLT_BDRT
+    chmod +x ./script/getBdRate.py && ./script/getBdRate.py script/anchor_100f_LP.log $CSTR_LOG_RLT_PSNR > $CSTR_LOG_RLT_BDRT
+
 done
+    # uodate enc time
+    chmod +x ./script/getEncTime.py && ./script/getEncTime.py script/anchor_time_100f_LP.log  $CSTR_LOG_RLT_time > $CSTR_LOG_RLT_ENCT
